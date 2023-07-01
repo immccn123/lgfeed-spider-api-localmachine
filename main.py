80608ef86ec4ddd0029b087c1e6bfeaa3e00c814
Imken Luo <me@imken.moe>
2023-07-01 10:32:23 +0800

    Add auth

--- main.py
+++ main.py
@@ -1,6 +1,25 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request, Response
 from routers import routes
 
-app = FastAPI()
+app = FastAPI(
+    title="Luogu Feed Tools",
+    description="懒得写了",
+    version="0.0.1",
+    contact={
+        "name": "Imken Luo",
+        "url": "https://imken.moe",
+        "email": "me@imken.moe",
+    },
+    redoc_url=None,
+)
+
+
+@app.middleware("http")
+async def auth(request: Request, call_next):
+    if request.headers.get("X-LGF-Auth") != "ImkenHaomeng":
+        return Response(status_code=403)
+    response = await call_next(request)
+    return response
+
 
 app.include_router(routes)
