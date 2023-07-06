57c0e727c077b98f9c37775ba7944674e0654f9c
Imken Luo <immccn123@outlook.com>
2023-07-06 20:49:00 +0800

    add daily dragon

--- routers/rank.py
+++ routers/rank.py
@@ -35,6 +35,30 @@ async def dragon_king():
     ]
 
 
+@app.get("/rank/dailyDragon")
+async def daily_dragon_king():
+    current_time = datetime.datetime.now()
+    offset = datetime.timedelta(days=-1)
+    return [
+        {
+            "uid": feed.user_id,
+            "name": feed.username,
+            "count": feed.cnt,
+        }
+        for feed in (
+            models.Feed.select(
+                models.Feed.user_id,
+                fn.MAX(models.Feed.username).alias("username"),
+                fn.COUNT(models.Feed.content).alias("cnt"),
+            )
+            .group_by(models.Feed.user_id, models.Feed.username)
+            .where(models.Feed.time >= (current_time + offset))
+            .order_by(-fn.COUNT(models.Feed.content).alias("cnt"))
+            .limit(100)
+        )
+    ]
+
+
 @app.get("/rank/tietie")
 async def tietie():
     current_time = datetime.datetime.now()
@@ -153,48 +177,6 @@ async def notificate_others():
                 "color": user_color[rank[1][0]],
             }
         )
-        if len(response) >= 100:
-            break
-    return response
-
-@app.get("/rank/csbzbsyy")
-async def chengshibuzubaoshiyouyu():
-    current_time = datetime.datetime.now()
-    offset = datetime.timedelta(days=-30)
-    user_count = {}
-    usernames = {}
-    user_color = {}
-    response = []
-    for feed in (
-        models.Feed.select(
-            models.Feed.username,
-            models.Feed.user_id,
-            models.Feed.user_color,
-            models.Feed.content,
-        )
-        .where(
-            (models.Feed.time >= (current_time + offset))
-            & models.Feed.content.contains("成事不足败事有余")
-        )
-        .order_by(models.Feed.time)
-    ):
-        if "成事不足败事有余" in feed.content.split("||")[0]:
-            if user_count.get(feed.user_id) is None:
-                user_count[feed.user_id] = 0
-            user_count[feed.user_id] += 1
-        usernames[feed.user_id] = feed.username
-        user_color[feed.user_id] = feed.user_color
-    user_count = sorted(user_count.items(), key=lambda kv: (kv[1], kv[0]), reverse=True)
-    for rank in enumerate(user_count):
-        if rank[1][1] >= 5:
-            response.append(
-                {
-                    "uid": rank[1][0],
-                    "count": rank[1][1],
-                    "name": usernames[rank[1][0]],
-                    "color": user_color[rank[1][0]],
-                }
-            )
         if len(response) >= 100:
             break
     return response
